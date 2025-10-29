34bdf0393bc2779fba028db4eb5f510712094395
Vijjo <vjjdaya@gmail.com>
2025-10-29 21:19:45 +0700

    style(stubs): Cập nhật 1 file .pyi (tự động bởi sgen)

--- modules/bootstrap/__init__.pyi
+++ modules/bootstrap/__init__.pyi
@@ -13,10 +13,16 @@ TEMPLATE_DIR: Any
 TYPE_HINT_MAP: Any
 TYPING_IMPORTS: Any
 execute_bootstrap_action: Any
+generate_bin_wrapper: Any
+generate_doc_file: Any
+generate_module_file: Any
+generate_module_init_file: Any
+generate_script_entrypoint: Any
 get_cli_args: Any
 load_bootstrap_config: Any
 load_spec_file: Any
 load_template: Any
+process_bootstrap_logic: Any
 
 # Static declaration of exported symbols (for Pylance)
 __all__: List[str] = [
@@ -29,8 +35,14 @@ __all__: List[str] = [
     'TYPE_HINT_MAP',
     'TYPING_IMPORTS',
     'execute_bootstrap_action',
+    'generate_bin_wrapper',
+    'generate_doc_file',
+    'generate_module_file',
+    'generate_module_init_file',
+    'generate_script_entrypoint',
     'get_cli_args',
     'load_bootstrap_config',
     'load_spec_file',
-    'load_template'
+    'load_template',
+    'process_bootstrap_logic'
 ]
