bfb88779855db4a641ff04f6bc2be1bbf8ec1d03
Vijjo <vjjdaya@gmail.com>
2025-10-30 20:24:59 +0700

    style(stubs): Cập nhật 1 file .pyi (tự động bởi sgen)

--- modules/no_doc/ndoc_internal/__init__.pyi
+++ modules/no_doc/ndoc_internal/__init__.pyi
@@ -7,6 +7,8 @@ from pathlib import Path
 analyze_file_content: Any
 load_config_files: Any
 merge_ndoc_configs: Any
+process_ndoc_task_dir: Any
+process_ndoc_task_file: Any
 scan_files: Any
 
 # Static declaration of exported symbols (for Pylance)
@@ -14,5 +16,7 @@ __all__: List[str] = [
     'analyze_file_content',
     'load_config_files',
     'merge_ndoc_configs',
+    'process_ndoc_task_dir',
+    'process_ndoc_task_file',
     'scan_files'
 ]
