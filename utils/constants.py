c4b1cf62027b83d97f2839ac63c7da3e8173b9cc
Vijjo <vjjdaya@gmail.com>
2025-10-31 03:49:19 +0700

    support pyi files

--- utils/constants.py
+++ utils/constants.py
@@ -19,6 +19,7 @@ FILE_LOG_LEVEL: Final[str] = "DEBUG"
 
 DEFAULT_EXTENSIONS_LANG_MAP: Final[Dict[str, str]] = {
     "py": "python",
+    "pyi": "python",
     "js": "javascript",
     "sh": "shell",
     "bash": "shell",
