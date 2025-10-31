8f39328bbc38231546af0b0470f0a192771da507
Vijjo <vjjdaya@gmail.com>
2025-10-31 08:57:13 +0700

    style(stubs): Cập nhật 2 file .pyi (tự động bởi sgen)

--- modules/no_doc/no_doc_internal/__init__.pyi
+++ modules/no_doc/no_doc_internal/__init__.pyi
@@ -4,7 +4,7 @@
 from typing import Any, List, Optional, Set, Dict, Union
 from pathlib import Path
 
-analyze_file_content: Any
+analyze_file_for_cleaning_and_formatting: Any
 load_config_files: Any
 merge_ndoc_configs: Any
 process_no_doc_task_dir: Any
@@ -13,7 +13,7 @@ scan_files: Any
 
 # Static declaration of exported symbols (for Pylance)
 __all__: List[str] = [
-    "analyze_file_content",
+    "analyze_file_for_cleaning_and_formatting",
     "load_config_files",
     "merge_ndoc_configs",
     "process_no_doc_task_dir",
