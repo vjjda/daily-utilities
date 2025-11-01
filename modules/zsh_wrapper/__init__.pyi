264c062c5444700580af702301c22ef1e6418635
Vijjo <vjjdaya@gmail.com>
2025-11-01 18:46:23 +0700

    style(stubs): Cập nhật 2 file .pyi (tự động bởi sgen)

--- modules/zsh_wrapper/__init__.pyi
+++ modules/zsh_wrapper/__init__.pyi
@@ -13,13 +13,6 @@ DEFAULT_WRAPPER_RELATIVE_DIR: Any
 PROJECT_CONFIG_FILENAME: Any
 TEMPLATE_FILENAME: Any
 ZRAP_DEFAULTS: Any
-execute_zsh_wrapper_action: Any
-find_project_root: Any
-generate_wrapper_content: Any
-resolve_default_output_path: Any
-resolve_output_path_interactively: Any
-resolve_root_interactively: Any
-resolve_wrapper_inputs: Any
 run_zsh_wrapper: Any
 
 # Static declaration of exported symbols (for Pylance)
@@ -33,12 +26,5 @@ __all__: List[str] = [
     "PROJECT_CONFIG_FILENAME",
     "TEMPLATE_FILENAME",
     "ZRAP_DEFAULTS",
-    "execute_zsh_wrapper_action",
-    "find_project_root",
-    "generate_wrapper_content",
-    "resolve_default_output_path",
-    "resolve_output_path_interactively",
-    "resolve_root_interactively",
-    "resolve_wrapper_inputs",
     "run_zsh_wrapper",
 ]
