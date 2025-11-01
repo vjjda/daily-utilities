4f16b60c165e819b81febe61f6a508468c626307
Vijjo <vjjdaya@gmail.com>
2025-11-01 18:56:15 +0700

    style(stubs): Cập nhật 2 file .pyi (tự động bởi sgen)

--- modules/bootstrap/__init__.pyi
+++ modules/bootstrap/__init__.pyi
@@ -12,26 +12,7 @@ DEFAULT_SCRIPTS_DIR_NAME: Any
 TEMPLATE_DIR: Any
 TYPE_HINT_MAP: Any
 TYPING_IMPORTS: Any
-build_argparse_arguments: Any
-build_args_pass_to_core: Any
-build_config_all_list: Any
-build_config_constants: Any
-build_config_imports: Any
-build_path_expands: Any
-build_typer_app_code: Any
-build_typer_args_pass_to_core: Any
-build_typer_main_signature: Any
-build_typer_path_expands: Any
 execute_bootstrap_action: Any
-generate_bin_wrapper: Any
-generate_doc_file: Any
-generate_module_file: Any
-generate_module_init_file: Any
-generate_script_entrypoint: Any
-get_cli_args: Any
-load_bootstrap_config: Any
-load_spec_file: Any
-load_template: Any
 process_bootstrap_logic: Any
 
 # Static declaration of exported symbols (for Pylance)
@@ -44,25 +25,6 @@ __all__: List[str] = [
     "TEMPLATE_DIR",
     "TYPE_HINT_MAP",
     "TYPING_IMPORTS",
-    "build_argparse_arguments",
-    "build_args_pass_to_core",
-    "build_config_all_list",
-    "build_config_constants",
-    "build_config_imports",
-    "build_path_expands",
-    "build_typer_app_code",
-    "build_typer_args_pass_to_core",
-    "build_typer_main_signature",
-    "build_typer_path_expands",
     "execute_bootstrap_action",
-    "generate_bin_wrapper",
-    "generate_doc_file",
-    "generate_module_file",
-    "generate_module_init_file",
-    "generate_script_entrypoint",
-    "get_cli_args",
-    "load_bootstrap_config",
-    "load_spec_file",
-    "load_template",
     "process_bootstrap_logic",
 ]
--- modules/bootstrap/bootstrap_internal/__init__.pyi
+++ modules/bootstrap/bootstrap_internal/__init__.pyi
@@ -4,51 +4,45 @@
 from typing import Any, List, Optional, Set, Dict, Union
 from pathlib import Path
 
-# Liệt kê TẤT CẢ các hàm đã bị ẩn đi
-# (Từ loader)
-load_template: Any
-load_bootstrap_config: Any
-load_spec_file: Any
-# (Từ generator)
-generate_bin_wrapper: Any
-generate_script_entrypoint: Any
-generate_module_file: Any
-generate_module_init_file: Any
-generate_doc_file: Any
-# (Từ utils)
-get_cli_args: Any
-# (Từ config_builder)
-build_config_constants: Any
+build_argparse_arguments: Any
+build_args_pass_to_core: Any
 build_config_all_list: Any
+build_config_constants: Any
 build_config_imports: Any
-# (Từ typer_builder)
+build_path_expands: Any
 build_typer_app_code: Any
-build_typer_path_expands: Any
 build_typer_args_pass_to_core: Any
 build_typer_main_signature: Any
-# (Từ argparse_builder)
-build_argparse_arguments: Any
-build_path_expands: Any
-build_args_pass_to_core: Any
+build_typer_path_expands: Any
+generate_bin_wrapper: Any
+generate_doc_file: Any
+generate_module_file: Any
+generate_module_init_file: Any
+generate_script_entrypoint: Any
+get_cli_args: Any
+load_bootstrap_config: Any
+load_spec_file: Any
+load_template: Any
 
+# Static declaration of exported symbols (for Pylance)
 __all__: List[str] = [
-    "load_template",
-    "load_bootstrap_config",
-    "load_spec_file",
-    "generate_bin_wrapper",
-    "generate_script_entrypoint",
-    "generate_module_file",
-    "generate_module_init_file",
-    "generate_doc_file",
-    "get_cli_args",
-    "build_config_constants",
+    "build_argparse_arguments",
+    "build_args_pass_to_core",
     "build_config_all_list",
+    "build_config_constants",
     "build_config_imports",
+    "build_path_expands",
     "build_typer_app_code",
-    "build_typer_path_expands",
     "build_typer_args_pass_to_core",
     "build_typer_main_signature",
-    "build_argparse_arguments",
-    "build_path_expands",
-    "build_args_pass_to_core",
+    "build_typer_path_expands",
+    "generate_bin_wrapper",
+    "generate_doc_file",
+    "generate_module_file",
+    "generate_module_init_file",
+    "generate_script_entrypoint",
+    "get_cli_args",
+    "load_bootstrap_config",
+    "load_spec_file",
+    "load_template",
 ]
