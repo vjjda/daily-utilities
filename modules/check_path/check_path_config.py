4a761786bdabae94a97c1133e43283def6424b81
Vijjo <vjjdaya@gmail.com>
2025-11-01 14:56:09 +0700

    remove "" files

--- modules/check_path/check_path_config.py
+++ modules/check_path/check_path_config.py
@@ -34,7 +34,6 @@ DEFAULT_EXTENSIONS: Final[Set[str]] = {
     "zsh",
     "sh",
     "template.toml",
-    "",
 }
 
 
@@ -79,5 +78,4 @@ COMMENT_RULES_BY_EXT: Final[Dict[str, Dict[str, Any]]] = {
     ".md": COMMENT_RULES["md_block"],
     ".py.template": COMMENT_RULES["hash_line"],
     ".template.toml": COMMENT_RULES["hash_line"],
-    "": COMMENT_RULES["hash_line"],
 }
