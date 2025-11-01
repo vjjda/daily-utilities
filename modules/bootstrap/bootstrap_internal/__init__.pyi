952572e3061a765f3e57ad81b6dad5caee43d3e0
Vijjo <vjjdaya@gmail.com>
2025-11-01 12:01:01 +0700

    fix(bootstrap): Thêm stub .pyi cho bootstrap_internal để fix Pylance

--- modules/bootstrap/bootstrap_internal/__init__.pyi
+++ modules/bootstrap/bootstrap_internal/__init__.pyi
@@ -1,28 +1,44 @@
 # Path: modules/bootstrap/bootstrap_internal/__init__.pyi
 """Statically declared API for bootstrap_internal"""
 
-from typing import Any, List, Optional, Set, Dict, Union
+from typing import Any, List, Optional, Set, Dict, Union, Tuple
 from pathlib import Path
+import logging
+import argparse
 
-generate_bin_wrapper: Any
-generate_doc_file: Any
-generate_module_file: Any
-generate_module_init_file: Any
-generate_script_entrypoint: Any
+# --- From bootstrap_loader ---
+load_template: Any
 load_bootstrap_config: Any
 load_spec_file: Any
-load_template: Any
+
+# --- From bootstrap_utils ---
+get_cli_args: Any
+
+# --- From bootstrap_generator ---
+generate_bin_wrapper: Any
+generate_script_entrypoint: Any
+generate_module_file: Any
+generate_module_init_file: Any
+generate_doc_file: Any
+
+# --- From bootstrap_orchestrator ---
 process_bootstrap_logic: Any
+
 
 # Static declaration of exported symbols (for Pylance)
 __all__: List[str] = [
-    "generate_bin_wrapper",
-    "generate_doc_file",
-    "generate_module_file",
-    "generate_module_init_file",
-    "generate_script_entrypoint",
+    # from bootstrap_loader
+    "load_template",
     "load_bootstrap_config",
     "load_spec_file",
-    "load_template",
+    # from bootstrap_utils
+    "get_cli_args",
+    # from bootstrap_generator
+    "generate_bin_wrapper",
+    "generate_script_entrypoint",
+    "generate_module_file",
+    "generate_module_init_file",
+    "generate_doc_file",
+    # from bootstrap_orchestrator
     "process_bootstrap_logic",
 ]
