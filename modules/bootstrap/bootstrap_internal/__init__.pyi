325b1b2dda4a1e142d0c50fd388b220e4e003e34
Vijjo <vjjdaya@gmail.com>
2025-11-01 11:58:16 +0700

    style(stubs): Cập nhật 2 file .pyi (tự động bởi sgen)

---
+++ modules/bootstrap/bootstrap_internal/__init__.pyi
@@ -0,0 +1,28 @@
+# Path: modules/bootstrap/bootstrap_internal/__init__.pyi
+"""Statically declared API for bootstrap_internal"""
+
+from typing import Any, List, Optional, Set, Dict, Union
+from pathlib import Path
+
+generate_bin_wrapper: Any
+generate_doc_file: Any
+generate_module_file: Any
+generate_module_init_file: Any
+generate_script_entrypoint: Any
+load_bootstrap_config: Any
+load_spec_file: Any
+load_template: Any
+process_bootstrap_logic: Any
+
+# Static declaration of exported symbols (for Pylance)
+__all__: List[str] = [
+    "generate_bin_wrapper",
+    "generate_doc_file",
+    "generate_module_file",
+    "generate_module_init_file",
+    "generate_script_entrypoint",
+    "load_bootstrap_config",
+    "load_spec_file",
+    "load_template",
+    "process_bootstrap_logic",
+]
