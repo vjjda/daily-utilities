21242c2d056ca27c1a425457ec5a87ba3d9fa4c8
Vijjo <vjjdaya@gmail.com>
2025-11-01 13:07:10 +0700

    style(stubs): Cập nhật 2 file .pyi (tự động bởi sgen)

--- modules/bootstrap/bootstrap_internal/__init__.pyi
+++ modules/bootstrap/bootstrap_internal/__init__.pyi
@@ -9,10 +9,6 @@ generate_doc_file: Any
 generate_module_file: Any
 generate_module_init_file: Any
 generate_script_entrypoint: Any
-get_cli_args: Any
-load_bootstrap_config: Any
-load_spec_file: Any
-load_template: Any
 
 # Static declaration of exported symbols (for Pylance)
 __all__: List[str] = [
@@ -21,8 +17,4 @@ __all__: List[str] = [
     "generate_module_file",
     "generate_module_init_file",
     "generate_script_entrypoint",
-    "get_cli_args",
-    "load_bootstrap_config",
-    "load_spec_file",
-    "load_template",
 ]
