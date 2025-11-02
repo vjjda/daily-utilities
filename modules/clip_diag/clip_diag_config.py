f8046eba2197406894d1057f3a47ff16af6e233f
Vijjo <vjjdaya@gmail.com>
2025-11-02 12:14:55 +0700

    delete blank spaces

--- modules/clip_diag/clip_diag_config.py
+++ modules/clip_diag/clip_diag_config.py
@@ -15,22 +15,15 @@ __all__ = [
 
 
 DEFAULT_OUTPUT_DIR: Final[Path] = Path(os.path.expanduser("~/Documents/graphviz"))
-
-
 DOT_PATH: Final[str] = "/opt/homebrew/bin/dot"
 MMC_PATH: Final[str] = "/opt/homebrew/bin/mmc"
-
-
 APP_CONFIG: Final[Dict[str, str]] = {
     "dot_app": "DotChart",
     "mermaid_app": "MarkChart",
     "svg_viewer_app": "Google Chrome",
     "png_viewer_app": "Preview",
 }
-
-
 GRAPHVIZ_PREFIX: Final[str] = "graphviz"
 MERMAID_PREFIX: Final[str] = "mermaid"
-
 
 DEFAULT_TO_ARG: Final[Optional[str]] = None
