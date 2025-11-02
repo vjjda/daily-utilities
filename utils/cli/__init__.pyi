70dd07de282583ab78e7441143473871fde3b381
Vijjo <vjjdaya@gmail.com>
2025-11-02 11:53:58 +0700

    style(stubs): Cập nhật 1 file (sgen) [Settings:6e0941f6a6]

--- utils/cli/__init__.pyi
+++ utils/cli/__init__.pyi
@@ -7,6 +7,7 @@ from pathlib import Path
 handle_config_init_request: Any
 handle_project_root_validation: Any
 launch_editor: Any
+print_grouped_report: Any
 prompt_config_overwrite: Any
 resolve_input_paths: Any
 resolve_reporting_root: Any
@@ -16,6 +17,7 @@ __all__: List[str] = [
     "handle_config_init_request",
     "handle_project_root_validation",
     "launch_editor",
+    "print_grouped_report",
     "prompt_config_overwrite",
     "resolve_input_paths",
     "resolve_reporting_root",
