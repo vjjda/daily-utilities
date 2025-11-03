6bdfc758128181d756377a48f49f41368a866c1c
Vijjo <vjjdaya@gmail.com>
2025-11-03 08:34:48 +0700

    refactor(check_path): Expose only core functionalities in __init__.py

--- modules/check_path/__init__.py
+++ modules/check_path/__init__.py
@@ -1,30 +1,8 @@
 # Path: modules/check_path/__init__.py
-from .check_path_config import (
-    DEFAULT_IGNORE,
-    DEFAULT_EXTENSIONS,
-    COMMENT_RULES,
-    COMMENT_RULES_BY_EXT,
-    PROJECT_CONFIG_FILENAME,
-    CONFIG_SECTION_NAME,
-    CONFIG_FILENAME,
-    MODULE_DIR,
-    TEMPLATE_FILENAME,
-    CPATH_DEFAULTS,
-)
 from .check_path_core import process_check_path_logic
 from .check_path_executor import execute_check_path_action
 
 __all__ = [
-    "DEFAULT_IGNORE",
-    "DEFAULT_EXTENSIONS",
-    "COMMENT_RULES",
-    "COMMENT_RULES_BY_EXT",
-    "PROJECT_CONFIG_FILENAME",
-    "CONFIG_SECTION_NAME",
-    "CONFIG_FILENAME",
-    "MODULE_DIR",
-    "TEMPLATE_FILENAME",
-    "CPATH_DEFAULTS",
     "process_check_path_logic",
     "execute_check_path_action",
 ]
