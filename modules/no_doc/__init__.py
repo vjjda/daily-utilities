505f805daa963b6e968892363a93eabecc8e4934
Vijjo <vjjdaya@gmail.com>
2025-11-03 08:36:30 +0700

    refactor(no_doc): Expose only necessary functionalities in __init__.py based on script usage

--- modules/no_doc/__init__.py
+++ modules/no_doc/__init__.py
@@ -1,9 +1,6 @@
 # Path: modules/no_doc/__init__.py
 from .no_doc_config import (
     DEFAULT_START_PATH,
-    DEFAULT_EXTENSIONS,
-    DEFAULT_IGNORE,
-    DEFAULT_FORMAT_EXTENSIONS,
     PROJECT_CONFIG_FILENAME,
     CONFIG_FILENAME,
     CONFIG_SECTION_NAME,
@@ -16,9 +13,6 @@ from .no_doc_executor import execute_ndoc_action
 
 __all__ = [
     "DEFAULT_START_PATH",
-    "DEFAULT_EXTENSIONS",
-    "DEFAULT_IGNORE",
-    "DEFAULT_FORMAT_EXTENSIONS",
     "PROJECT_CONFIG_FILENAME",
     "CONFIG_FILENAME",
     "CONFIG_SECTION_NAME",
