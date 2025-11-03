a4efebc0310a19d617b64fa0741bd149df9d904c
Vijjo <vjjdaya@gmail.com>
2025-11-03 08:47:28 +0700

    refactor(check_path): Adjust exposed functionalities in check_path_internal/__init__.py based on internal module usage

--- modules/check_path/check_path_internal/__init__.py
+++ modules/check_path/check_path_internal/__init__.py
@@ -2,16 +2,11 @@
 from .check_path_analyzer import analyze_single_file_for_path_comment
 from .check_path_loader import load_config_files
 from .check_path_merger import merge_check_path_configs
-from .check_path_rules import apply_line_comment_rule, apply_block_comment_rule
-from .check_path_scanner import scan_files
 from .check_path_task_dir import process_check_path_task_dir
 
 __all__ = [
     "analyze_single_file_for_path_comment",
     "load_config_files",
     "merge_check_path_configs",
-    "apply_line_comment_rule",
-    "apply_block_comment_rule",
-    "scan_files",
     "process_check_path_task_dir",
 ]
