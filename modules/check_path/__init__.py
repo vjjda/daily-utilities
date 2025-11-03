2c504a9ac195346b9c659ea401e7ab55d0c4a0c2
Vijjo <vjjdaya@gmail.com>
2025-11-03 08:44:28 +0700

    refactor(check_path): Adjust exposed functionalities in __init__.py based on tools/check_path.py usage

--- modules/check_path/__init__.py
+++ modules/check_path/__init__.py
@@ -1,8 +1,22 @@
 # Path: modules/check_path/__init__.py
+from .check_path_config import (
+    MODULE_DIR,
+    TEMPLATE_FILENAME,
+    CPATH_DEFAULTS,
+    PROJECT_CONFIG_FILENAME,
+    CONFIG_SECTION_NAME,
+    CONFIG_FILENAME,
+)
 from .check_path_core import process_check_path_logic
 from .check_path_executor import execute_check_path_action
 
 __all__ = [
     "process_check_path_logic",
     "execute_check_path_action",
+    "MODULE_DIR",
+    "TEMPLATE_FILENAME",
+    "CPATH_DEFAULTS",
+    "PROJECT_CONFIG_FILENAME",
+    "CONFIG_SECTION_NAME",
+    "CONFIG_FILENAME",
 ]
