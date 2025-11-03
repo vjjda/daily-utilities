5b8728983b841dd3b462134e3e45b40545b5c0eb
Vijjo <vjjdaya@gmail.com>
2025-11-03 08:35:51 +0700

    refactor(format_code): Expose only core functionalities in __init__.py

--- modules/format_code/__init__.py
+++ modules/format_code/__init__.py
@@ -1,28 +1,8 @@
 # Path: modules/format_code/__init__.py
-from .format_code_config import (
-    DEFAULT_START_PATH,
-    DEFAULT_EXTENSIONS,
-    DEFAULT_IGNORE,
-    PROJECT_CONFIG_FILENAME,
-    CONFIG_FILENAME,
-    CONFIG_SECTION_NAME,
-    MODULE_DIR,
-    TEMPLATE_FILENAME,
-    FORC_DEFAULTS,
-)
 from .format_code_core import process_format_code_logic
 from .format_code_executor import execute_format_code_action
 
 __all__ = [
-    "DEFAULT_START_PATH",
-    "DEFAULT_EXTENSIONS",
-    "DEFAULT_IGNORE",
-    "PROJECT_CONFIG_FILENAME",
-    "CONFIG_FILENAME",
-    "CONFIG_SECTION_NAME",
-    "MODULE_DIR",
-    "TEMPLATE_FILENAME",
-    "FORC_DEFAULTS",
     "process_format_code_logic",
     "execute_format_code_action",
 ]
