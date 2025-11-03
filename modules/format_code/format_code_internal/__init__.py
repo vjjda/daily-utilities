4c76085515b65b379a53df713ad27c78e13e70fe
Vijjo <vjjdaya@gmail.com>
2025-11-03 08:48:29 +0700

    refactor(format_code): Adjust exposed functionalities in format_code_internal/__init__.py based on internal module usage

--- modules/format_code/format_code_internal/__init__.py
+++ modules/format_code/format_code_internal/__init__.py
@@ -2,13 +2,11 @@
 from .format_code_analyzer import analyze_file_content_for_formatting
 from .format_code_loader import load_config_files
 from .format_code_merger import merge_format_code_configs
-from .format_code_scanner import scan_files
 from .format_code_task_dir import process_format_code_task_dir
 
 __all__ = [
     "analyze_file_content_for_formatting",
     "load_config_files",
     "merge_format_code_configs",
-    "scan_files",
     "process_format_code_task_dir",
 ]
